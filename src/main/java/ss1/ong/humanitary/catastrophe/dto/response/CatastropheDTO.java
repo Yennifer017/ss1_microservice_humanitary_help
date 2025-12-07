73414692b8b387c3021c261c8976a5b92711369e
Yennifer017 <yenniferdelion@gmail.com>
2025-12-07 17:35:10 -0600

    fix: catastropheDTO response value anotation added

--- src/main/java/ss1/ong/humanitary/catastrophe/dto/response/CatastropheDTO.java
+++ src/main/java/ss1/ong/humanitary/catastrophe/dto/response/CatastropheDTO.java
@@ -1,7 +1,9 @@
 package ss1.ong.humanitary.catastrophe.dto.response;
 
 import java.time.LocalDateTime;
+import lombok.Value;
 
+@Value
 public class CatastropheDTO {
     Integer id;
     String name;
