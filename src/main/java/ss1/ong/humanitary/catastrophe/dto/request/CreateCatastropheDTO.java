59a42ef48d44c6c5ebdb1d7448e94592d57d44ad
Yennifer017 <yenniferdelion@gmail.com>
2025-12-07 17:33:20 -0600

    fix: catastropheDTO value anotation added

--- src/main/java/ss1/ong/humanitary/catastrophe/dto/request/CreateCatastropheDTO.java
+++ src/main/java/ss1/ong/humanitary/catastrophe/dto/request/CreateCatastropheDTO.java
@@ -1,7 +1,9 @@
 package ss1.ong.humanitary.catastrophe.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
 
+@Value
 public class CreateCatastropheDTO {
     @NotBlank(message = "Debe especificarse el nombre")
     String name;
